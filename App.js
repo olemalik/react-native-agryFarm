661af6b7cd7b88e04a2122a7dd26194322f9a5ed
Malik Ahmed <olemalik@gmail.com>
2021-09-29 02:40:59 +0530

    Draw Navigation Implementation

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import * as React from 'react'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
+//import { useDrawerStatus } from '@react-navigation/drawer';
+ 
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        <Drawer.Navigator initialRouteName="Home">
+         <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Details" component={DetailsScreen} />
+      </Drawer.Navigator>
+      {/* <Stack.Navigator initialRouteName="Home" screenOptions={{
          headerStyle :{
           backgroundColor :   '#009387'         
           },
@@ -23,8 +30,8 @@ function App() {
          title: 'Overview'
         }} />
         <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      </Stack.Navigator> */}
+     </NavigationContainer>
   );
 }
     
