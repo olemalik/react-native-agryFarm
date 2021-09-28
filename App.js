160f43874e635d194625d902dd73c2bd5b0909c8
Malik Ahmed <olemalik@gmail.com>
2021-09-29 01:48:04 +0530

    Home and Details Screen Modifications

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Home')}
+      />
     </View>
   );
 }
@@ -18,11 +32,22 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+         headerStyle :{
+          backgroundColor :   '#009387'         
+          },
+          headerTintColor:'#fff', 
+          headerTitleStyle:{
+            fontWeight: 'bold'
+          }
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ 
+         title: 'Overview'
+        }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
+    
 export default App;
