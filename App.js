23f59a78ab03e7a223b0cc37718f47335ed62fdb
Malik Ahmed <olemalik@gmail.com>
2021-09-29 03:27:09 +0530

    Drawan open and Draow icon

--- App.js
+++ App.js
@@ -3,34 +3,65 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 //import { useDrawerStatus } from '@react-navigation/drawer';
- 
+
+import Icon from 'react-native-vector-icons'
+
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 
-const Stack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const DetailsStack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
+const HomeStackScreen=({ navigation })=> {
+  return (<HomeStack.Navigator initialRouteName="Home" screenOptions={{
+          headerStyle :{ 
+            backgroundColor :   '#009387'         
+           },
+          headerTintColor:'#fff',
+          headerTitleStyle:{ 
+            fontWeight: 'bold'
+           }
+      }}>
+      <HomeStack.Screen name="Home" component={HomeScreen} options={{ 
+        title: 'Overview', 
+        // headerLeft: () => (
+        //   <Icon.Button name='ios-menu' size={25} 
+        //    backgroundColor='#009387' onPress={() =>
+        //     navigation.openDrawer()} ></Icon.Button>
+        // )
+       }} /> 
+  </HomeStack.Navigator> 
+      )
+};
+const DetailsStackcreen=({ navigation })=> {
+ return(<DetailsStack.Navigator initialRouteName="Home" screenOptions={{
+         headerStyle :{ 
+           backgroundColor :   '#009387'         
+          },
+         headerTintColor:'#fff',
+         headerTitleStyle:{
+           fontWeight: 'bold'
+          }
+     }}> 
+   <DetailsStack.Screen name="Details" component={DetailsScreen} ptions={{ 
+        // headerLeft: () => (
+        //   <Icon.Button name='ios-menu' size={25} 
+        //    backgroundColor='#009387' onPress={() =>
+        //     navigation.openDrawer()} ></Icon.Button>
+        // )
+       }} />
+ </DetailsStack.Navigator> 
+    )
+};
 function App() {
   return (
     <NavigationContainer>
         <Drawer.Navigator initialRouteName="Home">
-         <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Details" component={DetailsScreen} />
+         <Drawer.Screen name="Home" component={HomeStackScreen} />
+          <Drawer.Screen name="Details" component={DetailsStackcreen} />
       </Drawer.Navigator>
-      {/* <Stack.Navigator initialRouteName="Home" screenOptions={{
-         headerStyle :{
-          backgroundColor :   '#009387'         
-          },
-          headerTintColor:'#fff', 
-          headerTitleStyle:{
-            fontWeight: 'bold'
-          }
-      }}>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ 
-         title: 'Overview'
-        }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator> */}
+      {/* */}
      </NavigationContainer>
   );
 }
