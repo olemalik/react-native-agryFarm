26a6f6bb1a0faccfc93b2525082926e9042658e4
Malik Ahmed <olemalik@gmail.com>
2021-09-30 02:28:25 +0530

    Drawer Navigation Content with Section

--- App.js
+++ App.js
@@ -4,13 +4,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';  
  
 import MainTabScreen from './screens/MainTabScreen';
+import {DrawerContent} from './screens/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 function App() {
+  // return (
+  //   <NavigationContainer DrawContent={props=> < DrawContent{...props}/>}>
+  //       <Drawer.Navigator initia lRouteName="Home">
+  //         <Drawer.Screen name="Home " component={MainTabScreen} />
+  //     </Drawer.Navigator>
+  //    </NavigationContainer>
+  // );
   return (
     <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
           <Drawer.Screen name="Home " component={MainTabScreen} />
       </Drawer.Navigator>
      </NavigationContainer>
