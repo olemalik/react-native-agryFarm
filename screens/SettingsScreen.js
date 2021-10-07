a4f6ff284461721dab0456750cfbf327bfb8a094
Malik Ahmed <olemalik@gmail.com>
2021-10-07 23:21:16 +0530

    Initial jitsi implementation

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import JitsiMeet, { JitsiMeetView } from 'react-native-jitsi-meet';
 
 const SettingsScreen = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Settings Screen</Text>
-        <Button
-          title="Click Here"
-          onPress={() => alert('Button Clicked!')}
-        />
-      </View>
-    );
+   
+  useEffect(() => {
+    setTimeout(() => {
+      const url = 'https://meet.jit.si/exemple';
+      const userInfo = {
+        displayName: 'User',
+        email: 'user@example.com',
+        avatar: 'https:/gravatar.com/avatar/abc123',
+      };
+      JitsiMeet.call(url, userInfo);
+      /* Você também pode usar o JitsiMeet.audioCall (url) para chamadas apenas de áudio */
+      /* Você pode terminar programaticamente a chamada com JitsiMeet.endCall () */
+    }, 1000);
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      JitsiMeet.endCall();
+    };
+  });
+
+  function onConferenceTerminated(nativeEvent) {
+    /* Conference terminated event */
+    console.log(nativeEvent)
+  }
+
+  function onConferenceJoined(nativeEvent) {
+    /* Conference joined event */
+    console.log(nativeEvent)
+  }
+
+  function onConferenceWillJoin(nativeEvent) {
+    /* Conference will join event */
+    console.log(nativeEvent)
+  }
+  return (
+    <JitsiMeetView
+      onConferenceTerminated={e => onConferenceTerminated(e)}
+      onConferenceJoined={e => onConferenceJoined(e)}
+      onConferenceWillJoin={e => onConferenceWillJoin(e)}
+      style={{
+        flex: 1,
+        height: '100%',
+        width: '100%',
+      }}
+    />
+  )
 };
-
 export default SettingsScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
-  },
-});
